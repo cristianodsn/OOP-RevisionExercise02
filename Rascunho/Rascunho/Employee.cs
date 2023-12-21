0b5430b4438fb8b93e9523bf0fecff26d516494f
Cristiano Castiel <cristianoct03@gmail.com>
2023-12-21 17:15:03 -0300

    OOP-exercise02

---
+++ Rascunho/Rascunho/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Rascunho
+{
+    class Employee
+    {
+        public string Name;
+        public double BaseSalay;
+        public double Tax;
+
+        public double NetSalary()
+        {
+            return BaseSalay - Tax;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            BaseSalay += BaseSalay / 100 * percentage;
+        }
+
+        public override string ToString()
+        {
+            return
+                Name + ", $ " + NetSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Rascunho/Rascunho/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee;
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Base salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Tax: ");
+            double tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            employee = new Employee { Name = name, BaseSalay = salary, Tax = tax };
+            Console.WriteLine("Employee: " + employee);
+
+            Console.Write("Enter the percentage to increase the salary: ");
+            double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            employee.IncreaseSalary(percentage);
+
+            Console.Write("Updated dada: " + employee);
+
+            Console.ReadKey();
+        }
+    }
+}
